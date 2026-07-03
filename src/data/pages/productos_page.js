c611acaa6e88b43ccc10e80f70101e56dfed122c
user <alanstevenson2005@gmail.com>
2026-07-02 19:16:30 -0600

    first commit

---
+++ src/data/pages/productos_page.js
@@ -0,0 +1,74 @@
+export default {
+    productos: [
+        {
+            prod_sku: "SKU-0001",
+            prod_nombre: "Laptop E14",
+            prod_precio: "18500.00",
+            prod_stock: 12,
+            prod_categoria: "Computadoras",
+        },
+        {
+            prod_sku: "SKU-0002",
+            prod_nombre: "Mouse Inalámbrico Logitech",
+            prod_precio: "350.00",
+            prod_stock: 48,
+            prod_categoria: "Accesorios",
+        },
+        {
+            prod_sku: "SKU-0003",
+            prod_nombre: "Teclado Redragon",
+            prod_precio: "890.00",
+            prod_stock: 20,
+            prod_categoria: "Accesorios",
+        },
+        {
+            prod_sku: "SKU-0004",
+            prod_nombre: "Monitor Samsung 24 Full HD",
+            prod_precio: "5200.00",
+            prod_stock: 8,
+            prod_categoria: "Monitores",
+        },
+        {
+            prod_sku: "SKU-0005",
+            prod_nombre: "Impresora HP",
+            prod_precio: "3100.00",
+            prod_stock: 5,
+            prod_categoria: "Impresoras",
+        },
+        {
+            prod_sku: "SKU-0006",
+            prod_nombre: "Disco Duro Externo 1TB",
+            prod_precio: "1450.00",
+            prod_stock: 15,
+            prod_categoria: "Almacenamiento",
+        },
+        {
+            prod_sku: "SKU-0007",
+            prod_nombre: "Memoria USB 64GB Kingston",
+            prod_precio: "280.00",
+            prod_stock: 60,
+            prod_categoria: "Almacenamiento",
+        },
+        {
+            prod_sku: "SKU-0008",
+            prod_nombre: "Webcam Logitech HD",
+            prod_precio: "1750.00",
+            prod_stock: 10,
+            prod_categoria: "Accesorios",
+        },
+        {
+            prod_sku: "SKU-0009",
+            prod_nombre: "Router C6",
+            prod_precio: "1200.00",
+            prod_stock: 18,
+            prod_categoria: "Redes",
+        },
+        {
+            prod_sku: "SKU-0010",
+            prod_nombre: "Silla GAMER de Oficina",
+            prod_precio: "4500.00",
+            prod_stock: 6,
+            prod_categoria: "Mobiliario",
+        },
+    ]
+};
